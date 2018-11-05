855a71026187c8c58fdfefa7c4a1d26a0d08e83d
Nestor Tamares <jrtamares@gmail.com>
2018-11-05 15:41:43 -0600

    Random Colors
    Random colors working correctly

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,10 @@
-var colors = ["rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(0, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var resultDisplay = document.getElementById("result");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,6 +20,7 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor) {
             resultDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         }
         else {
             this.style.backgroundColor = "#232323";
@@ -43,4 +39,19 @@ function changeColors(color) {
 function pickColor() {
     var randomColor = Math.floor(Math.random() * colors.length);
     return colors[randomColor];
+}
+
+function generateRandomColors(num) {
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
